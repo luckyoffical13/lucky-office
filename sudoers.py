a0a4ce75da495e6062f73b9f14de9c195ca6cc0c
luckyoffical13 <165084322+luckyoffical13@users.noreply.github.com>
2024-03-27 03:16:35 +0530

    Update sudoers.py

--- sudoers.py
+++ sudoers.py
@@ -1,12 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_sudo, remove_sudo
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.extraction import extract_user
-from AnonXMusic.utils.inline import close_markup
+from LuckyXMusic import app
+from LuckyXMusic.misc import SUDOERS
+from LuckyXMusic.utils.database import add_sudo, remove_sudo from LuckyXMusic.utils.decorators.language import language
+from LuckyXMusic.utils.extraction import extract_user
+from LuckyXMusic.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
 
 
