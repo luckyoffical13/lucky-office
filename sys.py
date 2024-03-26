f7872a33a923ef3d94e407762164fb94a0c67161
luckyoffical13 <165084322+luckyoffical13@users.noreply.github.com>
2024-03-27 03:13:59 +0530

    Update sys.py

--- sys.py
+++ sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from AnonXMusic.misc import _boot_
-from AnonXMusic.utils.formatters import get_readable_time
+from LuckyMusic.misc import _boot_
+from LuckyMusic.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
