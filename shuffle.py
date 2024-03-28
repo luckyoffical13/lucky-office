e860a5ca78fdfe9758b3e20c99acf19e0b6a636d
luckyoffical13 <165084322+luckyoffical13@users.noreply.github.com>
2024-03-28 16:17:05 +0530

    Update shuffle.py

--- shuffle.py
+++ shuffle.py
@@ -3,10 +3,10 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from LuckyXMusic import app
+from LuckyXMusic.misc import db
+from LuckyXMusic.utils.decorators import AdminRightsCheck
+from LuckyXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
